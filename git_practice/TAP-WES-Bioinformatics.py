77f84240e949ce128d2f068e853c9258abf2b00c
Nkosenhle Ndlovu <144995679+lindondlovu@users.noreply.github.com>
2026-07-22 15:30:16 +0200

    Add participant placeholders to bioinformatics register
    Added placeholders for multiple participants

--- git_practice/TAP-WES-Bioinformatics.py
+++ git_practice/TAP-WES-Bioinformatics.py
@@ -1,2 +1,16 @@
 Let’s mark the TAP-WES-Bioinformatics register!
-
+Participant 1: 
+Participant 2:
+Participant 3:
+Participant 4:
+Participant 5:
+Participant 6:
+Participant 7:
+Participant 8:
+Participant 9:
+Participant 10:
+Participant 11:
+Participant 12:
+Participant 13:
+Participant 14:
+Participant 15:
